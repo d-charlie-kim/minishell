f4829689723f5fcca7bc37681e6c477c0d52536e
charlie com <dokkim@student.42seoul.kr>
2021-12-06 03:43:29 +0900

    parsing almost done

--- includes/mijeong.h
+++ includes/mijeong.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mijeong.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaejeong <jaejeong@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dokkim <dokkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 16:55:04 by dokkim            #+#    #+#             */
-/*   Updated: 2021/12/05 17:32:30 by jaejeong         ###   ########.fr       */
+/*   Updated: 2021/12/06 03:31:16 by dokkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,10 @@ typedef struct s_info
 
 typedef struct s_process
 {
-	char *instruction;
-	char *option;
-	char **bundle;
+	int		token_count;
+	char	*instruction;
+	char	*option;
+	char	**bundle;
 }	t_process;
-
-
 
 # endif
--- srcs/mini_split.c
+++ srcs/mini_split.c
@@ -3,23 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   mini_split.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaejeong <jaejeong@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dokkim <dokkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 19:40:55 by dokkim            #+#    #+#             */
-/*   Updated: 2021/12/05 17:32:44 by jaejeong         ###   ########.fr       */
+/*   Updated: 2021/12/06 03:42:55 by dokkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mini_split.h"
 #include "mijeong.h"
+#include "mini_split.h"
 
+int	check_quotes(char c, int flag)
+{
+	if (c == '"' && flag < 2)
+		flag += 2;
+	else if (c == '"')
+		flag -= 2;
+	else if (c == '\'' && flag != 1 && flag != 3)
+		flag += 1;
+	else if (c == '\'')
+		flag -= 1;
+	return (flag);
+}
 
 int	token_size_check(char *ptr)
 {
-	int count;
+	int i;
+	int	flag;
+
+	i = 0;
+	flag = 0;
+	while()
+		;
 }
 
-int	redir_count(char *ptr, int size)
+int	token_count(char *ptr, int size)
 {
 	int i;
 	int count;
@@ -30,23 +48,17 @@ int	redir_count(char *ptr, int size)
 	flag = 0;
 	while (i < size)
 	{
-		if (ptr[i] == '"' || ptr[i] == '\'')
-		{
-			if (flag == 0)
-				flag = 1;
-			else
-				flag = 0;
-		}
-		if (flag == 0 && (ptr[i] == '>' || ptr[i] == '<'))
+		flag = check_quotes(ptr[i], flag);
+		if (flag == 0 && (ptr[i] == ' ' || ptr[i] == '>' || ptr[i] == '<'))
 		{
 			count++;
-			while (ptr[i] == '>' || ptr[i] == '<')
+			while (ptr[i] == ptr[i + 1])
 				i++;
-		}
-		else if (flag == 0 && ptr[i] == ' ')
-		{
-			count++;
-			while (ptr[i] == ' ')
+			if ((ptr[i] == '<' || ptr[i] == '>') && ptr[i + 1] == ' ')
+				i++;
+			else if (ptr[i] == ' ' && (ptr[i + 1] == '<' || ptr[i + 1] == '>'))
+				i++;
+			while (ptr[i] == ptr[i + 1])
 				i++;
 		}
 		i++;
@@ -54,11 +66,36 @@ int	redir_count(char *ptr, int size)
 	return (count);
 }
 
-void	second_split(t_process *bundle, char *ptr, int size)
+void	put_token(char *str, char *ptr, int size)
 {
-	int	token_count;
+	int i;
 
-	token_count = redir_count(ptr, size);
+	i = 0;
+	while (i < size)
+	{
+		str[i] = ptr[i];
+		ptr++;
+		i++;
+	}
+	str[i] = '\0';
+}
+
+void	second_split(t_process *process, char *ptr, int size)
+{
+	int i;
+	int	token_size;
+
+	i = 0;
+	token_size = 0;
+	process->token_count = token_count(ptr, size) + 1;
+	process->bundle = (char *)malloc(sizeof(char *) * (process->token_count));
+	while (i < process->token_count)
+	{
+		token_size = token_size_check(ptr);
+		process->bundle[i] = (char *)malloc(sizeof(char) * (token_size + 1));
+		put_token(&(process->bundle[i]), ptr, token_size);
+		i++;
+	}
 }
 
 int	pipe_size_check(char *ptr)
@@ -70,14 +107,16 @@ int	pipe_size_check(char *ptr)
 	flag = 0;
 	while (*ptr)
 	{
-		if (*ptr && (*ptr == '"' || *ptr == '\'')) // "'|'" -> 파이프로 인식해버림
-		{
-			if (flag == 0)
-				flag = 1;
-			else
-				flag = 0;
-		}
-		if (flag == 0 && *ptr == '|')
+		// if (*ptr == '"' && flag < 2)
+		// 	flag += 2;
+		// else if (*ptr == '"')
+		// 	flag -= 2;
+		// else if (*ptr == '\'' && flag != 1 && flag != 3)
+		// 	flag += 1;
+		// else if (*ptr == '\'')
+		// 	flag -= 1;
+		flag = check_quotes(*ptr, flag);
+		if (*ptr == '|' && flag == 0)
 		{
 			ptr++;
 			return (size);
@@ -88,7 +127,35 @@ int	pipe_size_check(char *ptr)
 	return (size);
 }
 
-int	pipe_count(char *str)
+// int	pipe_count(char *str)   // Option2. 플래그 두개로 처리하기
+// {
+// 	int i;
+// 	int count;
+// 	int	flag1;
+// 	int	flag2;
+
+// 	i = 0;
+// 	count = 0;
+// 	flag1 = 0;
+// 	flag2 = 0;
+// 	while (str[i])
+// 	{
+// 		if (*str == '"' && flag1 == 0)
+// 			flag1 = 1;
+// 		else if (*str == '"' && flag1 == 1)
+// 			flag1 = 0;
+// 		else if (*str =='\'' && flag2 == 0)
+// 			flag2 = 1;
+// 		else if (*str =='\'' && flag2 == 1)
+// 			flag2 = 0;
+// 		else if (flag1 == 0 && flag2 == 0 && str[i] == '|')
+// 			count++;
+// 		i++;
+// 	}
+// 	return (count);
+// }
+
+int	pipe_count(char *str)  // Option1. 플래그 하나로 처리하기
 {
 	int i;
 	int count;
@@ -99,14 +166,16 @@ int	pipe_count(char *str)
 	flag = 0;
 	while (str[i])
 	{
-		if (str[i] && (str[i] == '"' || str[i] == '\''))
-		{
-			if (flag == 0)
-				flag = 1;
-			else
-				flag = 0;
-		}
-		if (flag == 0 && str[i] == '|')
+		// if (str[i] == '"' && flag < 2)
+		// 	flag += 2;
+		// else if (str[i] == '"')
+		// 	flag -= 2;
+		// else if (str[i] == '\'' && flag != 1 && flag != 3)
+		// 	flag += 1;
+		// else if (str[i] == '\'')
+		// 	flag -= 1;
+		flag = check_quotes(str[i], flag);
+		if (str[i] == '|' && flag == 0)
 			count++;
 		i++;
 	}
