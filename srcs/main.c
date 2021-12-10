1a3a27809f5c2a9dd504386d274bc355b7d9ce7a
jaejeong <glaehfdl1654@naver.com>
2021-12-10 18:47:33 +0900

    add testcode

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,32 @@
 /*   By: jaejeong <jaejeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 16:02:24 by dokkim            #+#    #+#             */
-/*   Updated: 2021/12/08 01:01:47 by jaejeong         ###   ########.fr       */
+/*   Updated: 2021/12/10 17:43:46 by jaejeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mijeong.h"
 #include "parsing.h"
+
+void	print_parsing_data_test(t_process *processes, int process_count)
+{
+	t_list	*temp;
+
+	for (int i = 0; i < process_count; i++)
+	{
+		printf("%d번째 프로세스\n", i);
+		printf("명령어 : %s\n", processes[i].instruction);
+		printf("옵션 : \n");
+		temp = processes[i].option;
+		while (!temp)
+		{
+			char *str = (char *)temp->content;
+
+			printf("%s\n", str);
+			temp = temp->next;
+		}
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -26,7 +46,8 @@ int	main(int argc, char **argv, char **envp)
 	{
 		add_history(output);
 		processes = first_split(output, &info);
-		execute_instruction(&info, processes);
+		print_parsing_data_test(processes, info.process_count);
+		//execute_instruction(&info, processes);
 		free(output);
 	}
 	return (0);
