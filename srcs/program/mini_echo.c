e6386cb9a61ca5b9577b780872947862d29679a2
charlie com <dokkim@student.42seoul.kr>
2022-02-18 00:11:24 +0900

    [fix] : echo option fix

--- srcs/program/mini_echo.c
+++ srcs/program/mini_echo.c
@@ -28,25 +28,55 @@ void	printing_echo(t_list *ptr)
 	}
 }
 
+int	echo_option_print(t_process *process)
+{
+	t_list	*temp;
+	char	*option;
+	int		i;
+	int		ret;
+
+	ret = 1;
+	temp = process->option;
+	while (temp)
+	{
+		option = (char *)temp->content;
+		if (option[1] == 'n')
+		{
+			i = 1;
+			while (option[i] == 'n')
+				i++;
+			if (option[i] == '\0')
+			{
+				ret = 0;
+				temp = temp->next;
+				continue ;
+			}
+		}
+		else
+		{
+			printing_echo(temp);
+			return (ret);
+		}
+	}
+	return (ret);
+}
+
+void	echo_arg_print(t_process *process, int flag)
+{
+	if (process->arguments)
+	{
+		if (process->option && flag)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		printing_echo(process->arguments);
+	}
+}
+
 int	mini_echo(t_info *info, t_process *process)
 {
 	int		flag;
 
-	flag = 1;
-	if (process->option && ((char *)process->option->content)[1] == 'n' \
-			&& ((char *)process->option->content)[2] == '\0')
-	{
-		printing_echo(process->option->next);
-		flag = 0;
-	}
-	else
-		printing_echo(process->option);
-	if (process->arguments)
-	{
-		if (process->option)
-			ft_putchar_fd(' ', STDOUT_FILENO);
-		printing_echo(process->arguments);
-	}
+	flag = echo_option_print(process);
+	echo_arg_print(process, flag);
 	if (flag)
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	return (exit_process(info, 0));
